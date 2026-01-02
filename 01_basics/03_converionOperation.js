5bc1439ebc8e7670009fc2ac74b70475b14848ba
21rajveer <rajveersingh.ece27@gmail.com>
2026-01-02 12:45:21 +0000

    comparison js

--- 01_basics/03_converionOperation.js
+++ 01_basics/03_converionOperation.js
@@ -24,3 +24,41 @@ let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+       /**operations */
+
+let value=3;
+let negValue=-value
+//console.log(negValue)
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);*/
+
+/*let str1="hello"
+let str2=" rajveer"
+
+let str3=str1+str2;
+
+console.log(str3)
+
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"2");  //js conversion
+
+//console.log((3+4)*5%3);   question ke liye thik hai */
+
+//console.log(+true);
+//console.log(+""); //no value
+//operator precedance vo sirf exam ke liye
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gameCounter=100
+++gameCounter;  //prefix , post fix
+console.log(gameCounter)
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+console.log(2>1); //comparison opearators
+console.log(2>=1);
+console.log(2==2);
+console.log(2<1);
+console.log(2!=1); //same ho toh shi hai
+
+console.log("2">1);
+console.log("02">1); //fasne ke chance hai data type change 
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0); //values conversion problem 
+
+console.log(undefined==0)
+console.log(undefined==undefined)
+
+// === strict check 
+
+console.log("2"===2); //data type hi laag hai
