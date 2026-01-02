8c41fab0aa3f678cf47761fcbe5d1b00bb222580
21rajveer <rajveersingh.ece27@gmail.com>
2026-01-02 07:21:43 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const account_Id=144553 //change nhi kiya ja skta
+let accountEmail="hitesh@goggle.com"
+var accountPassword="12345";
+accountCity="jaipur"
+let accountState;
+
+//account_Id=2 //this is not allowed
+//{
+    //scope bola jaata hai
+//}
+
+accountEmail="hc@hc.com"
+accountPassword="21212121"
+accountCity="bengaluru"
+
+console.log(account_Id);
+/*
+prefer not to use var beacuse of issue in block scope
+and functional scope*/
+
+console.table([account_Id,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all code as newer version of javascript
+
+//alert("hello")  //we are using nodejs, not browser
+
+//console.log(3
+ //   +
+  //  3); //code must be readable should be high
+
+
+  let name="hitesh"
+  let age=18;
+  let isLoggedIn=false //boolean
+  let state;
+
+  //number=>2 to power 53
+  //bigint
+  //string=> " "
+  //boolean=> true/false;
+  //null=> standalon value
+  //undefined;
+  //symbol=> unique
+
+  //object
+
+  console.log(typeof "rajveer"); //string
+  console.log(typeof null); //object
+  console.log(typeof undefined); //undefined
+
