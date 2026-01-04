f0cedc50cd6b246353656783b7b575dc2bbd7021
21rajveer <rajveersingh.ece27@gmail.com>
2026-01-04 10:23:47 +0000

    data type summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,42 @@
+// #primitive 
+//7 categories me hote hai  -> call by value copy me changes hote hai 
+//string ,number,boolean,null,undefined,symbol,BigInt
+
+//const score=100
+//const scoreValue=100.3
+
+const isLoggedIn=false; ///boolean type
+const outsideTemp=null; //empty hai
+//let userEmail; //undefined
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId);
+
+//const bigNumber=34567890123n
+typeof bigNumber
+
+
+
+
+// non primitive (refrence type)
+//arrays,objects,functions 
+
+const heroes=["shaktiman", "naagraj","doga"]
+  let myObj= { 
+    name: rajveer  //objects 
+    age: 22,
+
+}
+
+//function as variable treat
+
+const myFunction=function(){
+    console.log("hello  world");
+}
+
+console.log(typeof bigNumber);
+
+//type of val->null  -> object 
+//type of function   -> function object
